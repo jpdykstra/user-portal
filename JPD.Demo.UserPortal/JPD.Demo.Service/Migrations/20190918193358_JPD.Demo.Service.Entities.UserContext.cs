874462014ee1c921c633fb370947c6fc21833c37
Jeff Dykstra <jeffery.dykstra@gmail.com>
2019-09-18 15:36:32 -0400

    Created database migration to meet the technical requirement of using the EF Code First to talk to the database.

---
+++ JPD.Demo.UserPortal/JPD.Demo.Service/Migrations/20190918193358_JPD.Demo.Service.Entities.UserContext.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JPD.Demo.Service.Migrations
+{
+    public partial class JPDDemoServiceEntitiesUserContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "Address",
+                schema: "dbo",
+                columns: table => new
+                {
+                    AddressId = table.Column<Guid>(nullable: false),
+                    AddressLine1 = table.Column<string>(maxLength: 250, nullable: false),
+                    AddressLine2 = table.Column<string>(maxLength: 250, nullable: true),
+                    City = table.Column<string>(maxLength: 250, nullable: false),
+                    State = table.Column<string>(maxLength: 2, nullable: false),
+                    ZipCode = table.Column<string>(maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.AddressId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                schema: "dbo",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(nullable: false),
+                    FirstName = table.Column<string>(maxLength: 75, nullable: false),
+                    LastName = table.Column<string>(maxLength: 75, nullable: false),
+                    Age = table.Column<short>(nullable: false),
+                    AddressId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_User_Address_AddressId",
+                        column: x => x.AddressId,
+                        principalSchema: "dbo",
+                        principalTable: "Address",
+                        principalColumn: "AddressId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Interest",
+                schema: "dbo",
+                columns: table => new
+                {
+                    InterestId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    Description = table.Column<string>(maxLength: 250, nullable: false),
+                    UserId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Interest", x => x.InterestId);
+                    table.ForeignKey(
+                        name: "FK_Interest_User_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "dbo",
+                        principalTable: "User",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Interest_UserId",
+                schema: "dbo",
+                table: "Interest",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_AddressId",
+                schema: "dbo",
+                table: "User",
+                column: "AddressId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Interest",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "User",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Address",
+                schema: "dbo");
+        }
+    }
+}
