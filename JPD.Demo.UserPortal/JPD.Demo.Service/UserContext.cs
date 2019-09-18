6e0ea25f0ace7770fe2ff4f0f177acdd5359fddc
Jeff Dykstra <jeffery.dykstra@gmail.com>
2019-09-18 15:20:34 -0400

    Added User Database Context for EF.

---
+++ JPD.Demo.UserPortal/JPD.Demo.Service/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using JPD.Demo.Service.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JPD.Demo.Service
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Address> Addresses { get; set; }
+
+        public DbSet<Interest> Interests { get; set; }
+    }
+}
