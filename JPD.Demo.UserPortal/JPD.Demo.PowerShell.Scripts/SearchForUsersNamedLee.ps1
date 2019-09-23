8206052aa568080f3d733858dd39148a8bfff11b
Jeff Dykstra <jeffery.dykstra@gmail.com>
2019-09-23 11:51:31 -0400

    Added comments to the PowerShell script.

--- JPD.Demo.UserPortal/JPD.Demo.PowerShell.Scripts/SearchForUsersNamedLee.ps1
+++ JPD.Demo.UserPortal/JPD.Demo.PowerShell.Scripts/SearchForUsersNamedLee.ps1
@@ -1,4 +1,6 @@
 ﻿#
-# Script.ps1
+# This script will search for users with a first or last name of Lee.
 #
+# NOTE: The JPD.Demo.Service project needs to be running in order for this script to connect to the server.
+
 Invoke-RestMethod -Uri 'https://localhost:44389/api/Users/searchbyname/lee' -Method Get
